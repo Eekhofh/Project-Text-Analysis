bcf4eb93ca501155a30fe3d88cd5ceaa648f33f4
LouisJavi21 <l.j.speelman.1@student.rug.nl>
2021-04-26 13:42:55 +0200

    Added the manual POS exercise

--- assignment2.py
+++ assignment2.py
@@ -26,6 +26,72 @@ def coll_chi(text_file):
 	sorted(bigram for bigram, score in scored)
 	return scored[0:19]
 
+
+def manual_pos():
+	print(
+		'''
+		Penn Treebank POS tagset
+		Peter: NN
+		really: RB
+		liked: VBD
+		the: DT
+		movies: NN
+		and: CC
+		warm: JJ
+		pop-corn: NN
+		He: PRP
+		would: VBD
+		never: RB
+		bring: VBD
+		Mira: NN
+		with: IN
+		him: PRP
+		though:
+		
+		Brown Corpus Tagset
+		Peter: NN
+		really: RB
+		liked: VBD
+		the: AT
+		movies: NNS
+		and: CC
+		warm: JJ
+		pop-corn: NN
+		.: .
+		He: PPS
+		would: MD
+		never: RB
+		bring: VB
+		Mira: NN
+		with: IN
+		him: PPO
+		,: ,
+		though: RB
+		.: .
+		
+		NLTK Universal Tagset
+		Peter: NOUN
+		really: ADV
+		liked: VERB
+		the: DET
+		movies: NOUN
+		and: CONJ
+		warm: ADJ
+		pop-corn: NOUN
+		.: .
+		He: NOUN
+		would: VERB
+		never: ADV
+		bring: VERB
+		Mira: NOUN
+		with: ADP
+		him: NOUN
+		,: .
+		though: ADJ
+		.: .
+		''')
+
+
 def main():
 	pmiscores = coll_pmi(sys.argv[1])
 	print("20 most like collocations using Pointwise Mutual Information:\n")
@@ -36,6 +102,8 @@ def main():
 	print(chiscores)
 	print()
 	print("Any differences might be explained by differences in the ways PMI and Chi-Sq. are calculated\n")
+	manual_pos()
+	print()
 	
 	
 if __name__ == "__main__":
