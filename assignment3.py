c57905fe7516c6df225c2668fc7583c2978646a3
JoeyMallat <joeymallat.business@hotmail.com>
2021-05-04 20:17:58 +0200

    Assignment 1.3

--- assignment3.py
+++ assignment3.py
@@ -66,21 +66,48 @@ def ner_wordnet(text_file):
 		print(ne_chunk(pos_text))
 
 
+def wordnet_similarity(text_file):
+	ranking = {}
+	with open(text_file, 'r') as infile:
+		text = infile.read()
+	car_automobile = ('car', wn.synset('car.n.01'),'automobile', wn.synset('automobile.n.01'))
+	coast_shore = ('coast', wn.synset('coast.n.01'), 'shore', wn.synset('shore.n.01'))
+	food_fruit = ('food', wn.synset('food.n.01'), 'fruit', wn.synset('fruit.n.01'))
+	journey_car = ('journey', wn.synset('journey.n.01'), 'car', wn.synset('car.n.01'))
+	monk_slave = ('monk', wn.synset('monk.n.01'), 'slave', wn.synset('slave.n.01'))
+	moon_string = ('moon', wn.synset('moon.n.01'), 'string', wn.synset('string.n.01'))
+	pairs = [car_automobile, coast_shore, food_fruit, journey_car, monk_slave, moon_string]
+	for pair in pairs:
+		score = pair[1].wup_similarity(pair[3])
+		pair_name = f'{pair[0]}-{pair[2]}'
+		ranking[pair_name] = score
+
+	sorted_ranking = dict(sorted(ranking.items(), key=lambda pair: pair[1], reverse=True))
+	for pair, score in sorted_ranking.items():
+		print(pair, score)
+	print('The biggest difference between the two rankings is the switch between the medium-level similarity and the low-level similarity.')
+	print('The top level remains the same by a large difference in score.')
+
 
 def main():
 	text = sys.argv[1]
 	data1 = question1(sys.argv[1])
-	print("Question 1")
+	print("Exercise 1.1")
+	print()
 	print(str(data1[0]) + " word(s) refer to a relative")
 	print("These words are: " + ", ".join(data1[1]))
 	print(str(data1[2]) + " word(s) refer to an illness")
 	print("These words are: " + ", ".join(data1[3]))
 	print(str(data1[4]) + " word(s) refer to a science")
 	print("These words are: " + ", ".join(data1[5]))
-	print('Exercise 2')
+	print('Exercise 1.2')
 	# Do not run without server!
 	#ner_wordnet(text)
-	
+	print()
+	print('Exercise 1.3')
+	print()
+	wordnet_similarity(sys.argv[1])
+
 
 if __name__ == '__main__':
 	main()
